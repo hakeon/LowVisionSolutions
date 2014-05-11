ae8d59840eaa7bdb4fe04e4c84eaf43f95a01a73
Brad J <hakeon@gmail.com>
2014-05-11 19:56:12 -0400

    megaMenu.js implimented
    successful on Firefox, Safari, and Opera: not functioning on style2 on
    Chrome

--- inc/htmlhead.php
+++ inc/htmlhead.php
@@ -2,8 +2,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
-	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
     <script src="js/jquery-accessibleMegaMenu.js"></script>
 	<?php
 	$thestyle = $_GET['set'];
