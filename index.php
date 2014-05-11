aff7d08e1e8ac5134a51b14e685e540cc816402e
Brad J <hakeon@gmail.com>
2014-05-11 19:41:15 -0400

    megaMenu CSS

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<?php include("inc/htmlhead.php");?>
+</head>
+<body id="home">
+<div id="container">
+	<?php include("inc/header.php");?>
+	<?php include("inc/nav.php");?>
+
+  <h1>Conference report</h1>
+  <p>Last week's conference presented an impressive line-up of speakers...</p>
+  <?php include("inc/footer.php");?>
+</div>	
+</body>
+</html>
