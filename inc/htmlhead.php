046ad4579a33d15baddc70595f44e2f48607a795
Brad J <hakeon@gmail.com>
2014-05-10 20:50:49 -0400

    PHP Style Switcher | htmlhead.php
    corrected and functional

--- inc/htmlhead.php
+++ inc/htmlhead.php
@@ -1,8 +1,19 @@
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" media="screen" href="<?php echo($thestyle);?>.css" >
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<?php
+	$thestyle = $_GET['set'];
+	if ($thestyle == "style1") {
+		$thestyle = "style2";
+	}
+	else {
+		$thestyle = "style1";
+	}
+	?>
+	<link rel="stylesheet" type="text/css" media="screen" href="css/<?php echo($thestyle);?>.css" >
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -10,15 +21,4 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
-<?php
-$thestyle = $_GET['set'];
-if ($thestyle == "style1")
-	{
-	$thestyle = "style2";
-	}
-else
-	{
-	$thestyle = "style1";
-	}
-?>
+	<title>Low Vision Solutions | Certified Low Vision Occupational Therapy</title>
