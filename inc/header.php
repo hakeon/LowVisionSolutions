b11c72898cf60117104ba51658d72d316fe18dc4
Brad J <hakeon@gmail.com>
2014-05-11 23:11:32 -0400

    updated style color alts

--- inc/header.php
+++ inc/header.php
@@ -1,9 +1,9 @@
 <div id="colorswitch">
 <p>Change colors:</p>
 	<ul class="inline">
-		<li><a href="#" onClick="javascript:changeColors('css/altColors1.css');return false;" id="altColors1">dark blue on chartreuse</a></li>
-		<li><a href="#" onClick="javascript:changeColors('css/altColors2.css');return false;" id="altColors2">yellow on black</a></li>
-		<li><a href="#" onClick="javascript:changeColors('css/altColors3.css');return false;" id="altColors3">black on pale yellow</a></li>
+		<li><a href="#" onClick="javascript:changeColors('css/altColors1.css');return false;" id="altColors1">blue on green</a></li>
+		<li><a href="#" onClick="javascript:changeColors('css/altColors2.css');return false;" id="altColors2">green on blue</a></li>
+		<li><a href="#" onClick="javascript:changeColors('css/altColors3.css');return false;" id="altColors3">yellow on black</a></li>
 		<li><a href="#" onClick="javascript:changeColors('css/altColors4.css');return false;" id="altColors4">black on white</a></li>
 		<li><a href="#" onClick="javascript:changeColors('css/defaultColors.css');return false;" id="default">Reset to default</a></li>
 	</ul>
