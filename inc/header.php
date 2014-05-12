5fb41b0a9ddb0c02cd9c8da74b75983c8fb8f8bf
Brad J <hakeon@gmail.com>
2014-05-12 17:06:37 -0400

    saving point ~ similar status as last commit

--- inc/header.php
+++ inc/header.php
@@ -1,8 +1,8 @@
 <div id="colorswitch">
 <p>Color Options</p>
 	<ul class="inline">
-		<li><a href="#" onmouseover="javascript:changeColors('css/altColors.css');return false;" id="altColors">Hi-View Colors</a></li>
-		<li><a href="#" onmouseover="javascript:changeColors('css/style.css');return false;" id="default">Orignal Colors</a></li>
+		<li><a href="#" onclick="javascript:changeColors('css/altColors.css');return false;" id="altColors">Hi-View Colors</a></li>
+		<li><a href="#" onclick="javascript:changeColors('css/style.css');return false;" id="default">Orignal Colors</a></li>
 	</ul>
 </div>
 <h1 id="LVC">Low Vision Solutions</h1>
