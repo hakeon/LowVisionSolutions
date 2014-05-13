19fdceb0ba2c15d0d929d29eecd16ac61288816d
Brad J <hakeon@gmail.com>
2014-05-12 23:27:53 -0400

    introductory content for 3 pages
    home, home & life, AMD

--- AMD.php
+++ AMD.php
@@ -8,7 +8,47 @@
 <div id="container">
 	<?php include("inc/header.php");?>
 	<?php include("inc/nav.php");?>
-	<h1 id="contentPending">CONTENT PENDING</h1>
+	<div class="content">
+		<h3>What is AMD?</h3>
+		<p>
+			Age-Related Macular Degeneration, or AMD, is a gradual, progressive, painless deterioration of the macula, which is the small area in the center of the retina that gives us our detailed vision.  This is why someone with vision loss from macular degeneration may have trouble reading mail or newspapers but have no trouble spotting an object off to the side or while walking around, even in unfamiliar places.
+		</p>
+		<h3>What is Like to See With AMD?</h3>
+		<p>
+			Are you experiencing blurring or a blind spot in the center of your vision? Are you seeing blurry areas on a printed page? Do some straight lines appear wavy? Are there dark spaces or areas in the center of your vision? It might be age-related macular degeneration.
+		</p>
+		<h3>What Should I Do if I Think I May Have AMD?</h3>
+		<P>
+			Call your Doctor, see an Ophthalmologist, and contact Katherine Meade, Certified Low Vision Occupational Therapy Specialist, for a consult
+		</P>
+			<div id="emailPrompt">
+				<script type="text/javascript">
+					//<![CDATA[
+					<!--
+					var x="function f(x){var i,o=\"\",l=x.length;for(i=l-1;i>=0;i--) {try{o+=x.c" +
+					"harAt(i);}catch(e){}}return o;}f(\")\\\"function f(x,y){var i,o=\\\"\\\\\\\""+
+					"\\\\,l=x.length;for(i=0;i<l;i++){if(i==65)y+=i;y%=127;o+=String.fromCharCod" +
+					"e(x.charCodeAt(i)^(y++));}return o;}f(\\\"\\\\%- 1(#)<g=9%9+#>ypo5u>%=?g\\\\"+
+					"\\\\007~0?6\\\\\\\\014\\\\\\\\025\\\\\\\\rY\\\\\\\\017\\\\\\\\004\\\\\\\\02" +
+					"2\\\\\\\\016\\\\\\\\r)\\\\\\\\006\\\\\\\\004\\\\\\\\033\\\\\\\\033\\\\\\\\0" +
+					"07\\\\\\\\034\\\\\\\\031\\\\\\\\036\\\\\\\\034\\\\\\\\000\\\\\\\\033\\\\\\\\"+
+					"031\\\\\\\\003\\\\\\\\003\\\\\\\\021\\\\\\\\026\\\\\\\\024\\\\\\\\010R\\\\\\"+
+					"\\036\\\\\\\\021?rw&/#$<t\\\\\\\\013k\\\\\\\\000\\\\\\\\\\\"\\\\9o\\\\\\\\0" +
+					"068!:;;v\\\\\\\\00475//520\\\\\\\\177)\\\\\\\\017\\\\\\\\004\\\\\\\\014\\\\" +
+					"\\\\026\\\\\\\\010\\\\\\\\007\\\\\\\\023\\\\\\\\001\\\\\\\\006\\\\\\\\004K>" +
+					"\\\\\\\\010\\\\\\\\037\\\\\\\\032\\\\\\\\025\\\\\\\\002\\\\\\\\006/VU\\\\\\" +
+					"\\002\\\\\\\\036\\\\\\\\014\\\\\\\\025\\\\\\\\037F _=LHAH$QI'[LDO,HCNY]N1*V" +
+					"z~{r:syo{?TN\\\\\\\\002PAKB\\\\\\\\007IG\\\\\\\\nNALGC\\\\\\\\014\\\\\\\\03" +
+					"6S\\\\\\\\r\\\\\\\\026\\\\\\\\034\\\\\\\\r\\\\\\\\007\\\\\\\\003\\\"\\\\,65" +
+					")\\\"(f};)lo,0(rtsbus.o nruter};)i(tArahc.x=+o{)--i;0=>i;1-l=i(rof}}{)e(hct" +
+					"ac};l=+l;x=+x{yrt{)011=!)31/l(tAedoCrahc.x(elihw;lo=l,htgnel.x=lo,\\\"\\\"=" +
+					"o,i rav{)x(f noitcnuf\")"                                                    ;
+					while(x=eval(x));
+					//-->
+					//]]>
+				</script>
+			</div>
+	</div>
 	<?php include("inc/footer.php");?>
 </div>	
 </body>
--- lifeAdaptation.php
+++ lifeAdaptation.php
@@ -8,7 +8,57 @@
 <div id="container">
 	<?php include("inc/header.php");?>
 	<?php include("inc/nav.php");?>
-	<h1 id="contentPending">CONTENT PENDING</h1>
+	<div class="content">
+		<h3>What Do You Mean by Home and Life Adaptation?</h3>
+		<p>
+			Your home and lifestyle are who you are, and our goal is to keep you at home, living your life, as safely as possible.
+			But Low Vision can challenge you, so we have methods that have been successful by adapting lifestyles and home environments
+			to your particular Low Vision challenge.
+			<h3>That Sounds Extensive, and Therefore Expensive</h3>
+			<p>
+				Not necessarily!  In fact, most modifications and adaptations are inexpensive and simple such as:
+			</p>
+			<ul>
+				<li>picking up throw rugs and rerouting electrical cords</li>
+				<li>installing brighter, and more, lights and lightbulbs</li>
+				<li>marking edges of stairs, handrails, countertops, and other edges with bright tape</li>
+				<li>replacing sheer curtains with blinds to reduce glare</li>
+			</ul>
+			<p>There are just a few common, easy, inexpensive adaptations that have been helpful for many Low Vision challenges</p>
+			<h3>This is Important, How Do I Get a Home Evaluation Scheduled?</h3>
+			<p>
+				A safe home is a happy home, so contact Katherine Meade, Certified Low Vision Occupational Therapist Specialist, to
+				schedule your home evaluation today
+			</p>
+					<div id="emailPrompt">
+						<script type="text/javascript">
+							//<![CDATA[
+							<!--
+							var x="function f(x){var i,o=\"\",l=x.length;for(i=l-1;i>=0;i--) {try{o+=x.c" +
+							"harAt(i);}catch(e){}}return o;}f(\")\\\"function f(x,y){var i,o=\\\"\\\\\\\""+
+							"\\\\,l=x.length;for(i=0;i<l;i++){if(i==65)y+=i;y%=127;o+=String.fromCharCod" +
+							"e(x.charCodeAt(i)^(y++));}return o;}f(\\\"\\\\%- 1(#)<g=9%9+#>ypo5u>%=?g\\\\"+
+							"\\\\007~0?6\\\\\\\\014\\\\\\\\025\\\\\\\\rY\\\\\\\\017\\\\\\\\004\\\\\\\\02" +
+							"2\\\\\\\\016\\\\\\\\r)\\\\\\\\006\\\\\\\\004\\\\\\\\033\\\\\\\\033\\\\\\\\0" +
+							"07\\\\\\\\034\\\\\\\\031\\\\\\\\036\\\\\\\\034\\\\\\\\000\\\\\\\\033\\\\\\\\"+
+							"031\\\\\\\\003\\\\\\\\003\\\\\\\\021\\\\\\\\026\\\\\\\\024\\\\\\\\010R\\\\\\"+
+							"\\036\\\\\\\\021?rw&/#$<t\\\\\\\\013k\\\\\\\\000\\\\\\\\\\\"\\\\9o\\\\\\\\0" +
+							"068!:;;v\\\\\\\\00475//520\\\\\\\\177)\\\\\\\\017\\\\\\\\004\\\\\\\\014\\\\" +
+							"\\\\026\\\\\\\\010\\\\\\\\007\\\\\\\\023\\\\\\\\001\\\\\\\\006\\\\\\\\004K>" +
+							"\\\\\\\\010\\\\\\\\037\\\\\\\\032\\\\\\\\025\\\\\\\\002\\\\\\\\006/VU\\\\\\" +
+							"\\002\\\\\\\\036\\\\\\\\014\\\\\\\\025\\\\\\\\037F _=LHAH$QI'[LDO,HCNY]N1*V" +
+							"z~{r:syo{?TN\\\\\\\\002PAKB\\\\\\\\007IG\\\\\\\\nNALGC\\\\\\\\014\\\\\\\\03" +
+							"6S\\\\\\\\r\\\\\\\\026\\\\\\\\034\\\\\\\\r\\\\\\\\007\\\\\\\\003\\\"\\\\,65" +
+							")\\\"(f};)lo,0(rtsbus.o nruter};)i(tArahc.x=+o{)--i;0=>i;1-l=i(rof}}{)e(hct" +
+							"ac};l=+l;x=+x{yrt{)011=!)31/l(tAedoCrahc.x(elihw;lo=l,htgnel.x=lo,\\\"\\\"=" +
+							"o,i rav{)x(f noitcnuf\")"                                                    ;
+							while(x=eval(x));
+							//-->
+						//]]>
+					</script>
+				</div>
+		</p>
+	</div>
 	<?php include("inc/footer.php");?>
 </div>	
 </body>
